a4c9ca0d30301474ed567920eb440ae6c25fa2fc
s13676 <lukasz.klim92@gmail.com>
2016-10-29 13:21:55 +0200

    Update

---
+++ src/main/java/domain/Address.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Address {
+	private String Street;
+	private String City;
+	private int PostCode;
+	public String getStreet() {
+		return Street;
+	}
+	public void setStreet(String street) {
+		Street = street;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public int getPostCode() {
+		return PostCode;
+	}
+	public void setPostCode(int postCode) {
+		PostCode = postCode;
+	}
+	
+	
+}
